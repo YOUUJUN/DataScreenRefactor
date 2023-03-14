06e749da042eea9ec4e9ecd9d880bbeade02fabf
kehan <kehan@fanmiot.cn>
2023-03-14 14:18:28 +0800

    feat: 添加滞留告警处理逻辑

--- src/api/dict.js
+++ src/api/dict.js
@@ -8,38 +8,40 @@ export function getAudioUrl(name) {
         device: "", // 设备离线
         flow: require("@/static/media/water_flow.mp3"), // 水流异常
         emergency: require("@/static/media/urgent.mp3"), // 紧急呼叫
-        intelligent_call : require("@/static/media/urgent.mp3"), // 智能呼叫
+        intelligent_call: require("@/static/media/urgent.mp3"), // 智能呼叫
         heart: require("@/static/media/heart.mp3"), // 心率异常
         breath: require("@/static/media/breath.mp3"), // 呼吸异常
+        retention: require("@/static/media/retention.mp3"), //滞留告警
         turn: "",
     };
 
-    if(audioUrl[name]){
-        return `${audioUrl[name]}`
-    }else{
-        return ""
+    if (audioUrl[name]) {
+        return `${audioUrl[name]}`;
+    } else {
+        return "";
     }
 }
 
 export function getWarnImgUrl(name) {
     const warnPic = {
-        "跌倒告警": require("@/static/warning/fall.png"),
-        "离床未归": require("@/static/warning/out.png"),
-        "烟雾告警": require("@/static/warning/smoke.png"),
-        "燃气告警": require("@/static/warning/gas.png"),
-        "用水异常": require("@/static/warning/abnormal.png"),
-        "设备离线": require("@/static/warning/device.png"),
-        "水流异常": require("@/static/warning/flow.png"),
-        "紧急呼叫": require("@/static/warning/emergency.png"),
-        "智能呼叫": require("@/static/warning/emergency.png"),
-        "心率异常": require("@/static/warning/heart.png"),
-        "呼吸异常": require("@/static/warning/breath.png"),
-        "翻身护理": require("@/static/warning/turn.png"),
+        跌倒告警: require("@/static/warning/fall.png"),
+        离床未归: require("@/static/warning/out.png"),
+        烟雾告警: require("@/static/warning/smoke.png"),
+        燃气告警: require("@/static/warning/gas.png"),
+        用水异常: require("@/static/warning/abnormal.png"),
+        设备离线: require("@/static/warning/device.png"),
+        水流异常: require("@/static/warning/flow.png"),
+        紧急呼叫: require("@/static/warning/emergency.png"),
+        智能呼叫: require("@/static/warning/emergency.png"),
+        心率异常: require("@/static/warning/heart.png"),
+        呼吸异常: require("@/static/warning/breath.png"),
+        翻身护理: require("@/static/warning/turn.png"),
+        滞留告警: require("@/static/warning/lingeringAlarm.png"),
     };
 
-    if(warnPic[name]){
-        return `${warnPic[name]}`
-    }else{
-        return ""
+    if (warnPic[name]) {
+        return `${warnPic[name]}`;
+    } else {
+        return "";
     }
 }
